ee43a9e6acdeb005b0cef0234dd7b84848dde7e8
礼检 <ljj337009@antgroup.com>
2021-10-09 10:22:17 +0800

    fix: delete unuse code

--- cli.js
+++ cli.js
@@ -1,1 +1,9 @@
 #!/usr/bin/env node
+
+const inquirer = require('inquirer');
+const downloadTemplate = require('./download.js');
+const question = require ('./question/index.js');
+
+inquirer.prompt(question).then((answers) => {
+  downloadTemplate(answers.framework, answers.version, answers.name);
+});
--- index.js
+++
@@ -1,7 +0,0 @@
-const inquirer = require('inquirer');
-const downloadTemplate = require('./download.js');
-const question = require ('./question/index.js');
-
-inquirer.prompt(question).then((answers) => {
-  downloadTemplate(answers.framework, answers.version, answers.name);
-});
